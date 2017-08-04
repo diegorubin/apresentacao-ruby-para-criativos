9159b6fef997b7cebc10d39abfc4ab964a92731a
Diego Rubin <rubin.diego@gmail.com>
2017-08-04 07:48:25 -0300

    adicionando novos slides.

--- examples/httpmocks/server.rb
+++ examples/httpmocks/server.rb
@@ -2,8 +2,6 @@ require "sinatra"
 
 get '/api/:name' do
   content_type 'plain/text'
-
-  puts File.exists?("files/#{params[:name]}")
   if File.exists?("files/#{params[:name]}")
     File.open("files/#{params[:name]}").read
   else
